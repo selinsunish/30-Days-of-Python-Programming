9986c877c6bfcb7b574a6ebe474e0f7f80d079c6
AnnliaJose <annliajose2028@cs.sjcetpalai.ac.in>
2024-11-21 22:17:02 +0530

    Add files via upload

---
+++ Day-1/a.py
@@ -0,0 +1,40 @@
+Age = 19
+print(Age)
+
+DAYS_IN_A_YEAR = 365
+WEEKS_IN_A_YEAR = 52
+MONTHS_IN_A_YEAR = 12
+years = 27
+total_days = years * DAYS_IN_A_YEAR
+total_weeks = years * WEEKS_IN_A_YEAR
+total_months = years * MONTHS_IN_A_YEAR
+print(f"Total days in {years} years: {total_days} days")
+print(f"Total weeks in {years} years: {total_weeks} weeks")
+print(f"Total months in {years} years: {total_months} months")
+
+radius = 5
+import math
+area = math.pi * radius ** 2
+print(f"The area of the circle of radius {radius} units is: {area} sq units.")
+
+name = input("Enter your name: ")
+department = input("Enter your department: ")
+rollno = input("Enter your roll number: ")
+print(f"Name: {name}, Department: {department}, Roll No: {rollno}")
+
+n1 = int(input("enter 1st no:"))
+n2 = int(input("enter 2nd no: "))
+sum = n1 + n2
+difference = n1 - n2
+product = n1 * n2
+remainder = n1%n2
+division = n1/ n2
+floor_division = n1 // n2
+
+print(f"Sum: {sum}")
+print(f"Difference: {difference}")
+print(f"Product: {product}")
+print(f"Remainder (Modulus): {remainder}")
+print(f"Division: {division}")
+print(f"Floor Division: {floor_division}")
+
