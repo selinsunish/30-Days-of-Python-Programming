47de1ebd0ab1591e3b7095844a87397b6a39d39e
Alan-s-benny <alansbenny2028@cs.sjcetpalai.ac.in>
2024-11-22 06:43:20 +0530

    Update day1 .py

--- day1 .py
+++ day1 .py
@@ -18,24 +18,18 @@ print("Months ",months)
 name = input("Enter your name: ")
 department = input("Enter your department: ")
 roll no = input("Enter your roll number: ")
-
 print("Name:",name)
 print("Department:", department)
 print("Roll Number:",roll no)
 
-
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-
-
 sum = num1 + num2
 difference = num1 - num2
 product = num1 * num2
 remainder = num1 % num2
 division = num1 / num2
 floor division = num1 // num2
-
-
 print("Sum:",sum)
 print("Difference:", difference)
 print("Product:", product)
