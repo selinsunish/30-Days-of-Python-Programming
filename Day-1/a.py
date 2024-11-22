c61e9063ec18b2daab4b8a14c58fb4ea207581ba
AnnliaJose <annliajose2028@cs.sjcetpalai.ac.in>
2024-11-22 20:24:29 +0530

    Update a.py

--- Day-1/a.py
+++ Day-1/a.py
@@ -1,6 +1,8 @@
+#1st question
 Age = 19
 print(Age)
 
+#2nd question
 DAYS_IN_A_YEAR = 365
 WEEKS_IN_A_YEAR = 52
 MONTHS_IN_A_YEAR = 12
@@ -8,20 +10,23 @@ years = 27
 total_days = years * DAYS_IN_A_YEAR
 total_weeks = years * WEEKS_IN_A_YEAR
 total_months = years * MONTHS_IN_A_YEAR
-print(f"Total days in {years} years: {total_days} days")
-print(f"Total weeks in {years} years: {total_weeks} weeks")
-print(f"Total months in {years} years: {total_months} months")
+print("Total days in {years} years: {total_days} days")
+print("Total weeks in {years} years: {total_weeks} weeks")
+print("Total months in {years} years: {total_months} months")
 
+#3rd question
 radius = 5
 import math
 area = math.pi * radius ** 2
-print(f"The area of the circle of radius {radius} units is: {area} sq units.")
+print("The area of the circle of radius {radius} units is: {area} sq units.")
 
+#4th question
 name = input("Enter your name: ")
 department = input("Enter your department: ")
 rollno = input("Enter your roll number: ")
-print(f"Name: {name}, Department: {department}, Roll No: {rollno}")
+print("Name: {name}, Department: {department}, Roll No: {rollno}")
 
+#5th question
 n1 = int(input("enter 1st no:"))
 n2 = int(input("enter 2nd no: "))
 sum = n1 + n2
@@ -30,11 +35,10 @@ product = n1 * n2
 remainder = n1%n2
 division = n1/ n2
 floor_division = n1 // n2
-
-print(f"Sum: {sum}")
-print(f"Difference: {difference}")
-print(f"Product: {product}")
-print(f"Remainder (Modulus): {remainder}")
-print(f"Division: {division}")
-print(f"Floor Division: {floor_division}")
+print("Sum: {sum}")
+print("Difference: {difference}")
+print("Product: {product}")
+print("Remainder (Modulus): {remainder}")
+print("Division: {division}")
+print("Floor Division: {floor_division}")
 
