5ffd47daeadf95688e18bdd1b8ab4475367eeefd
AnnliaJose <annliajose2028@cs.sjcetpalai.ac.in>
2024-11-24 13:39:36 +0530

    Add files via upload

---
+++ d3.py
@@ -0,0 +1,44 @@
+#1st question
+import math
+number = float(input("Enter a number: "))
+sqrt_number = math.sqrt(number)
+print("The square root of {number} is {sqrt_number}")
+import math
+
+#2nd question
+number = int(input("Enter a number: "))
+factorial = math.factorial(number)
+print("The factorial of {number} is {factorial}")
+
+#3rd question
+import math
+side1 = float(input("Enter the length of the first side: "))
+side2 = float(input("Enter the length of the second side: "))
+hypotenuse = math.hypot(side1, side2)
+print("The length of the hypotenuse is {hypotenuse}")
+
+#4th question
+import math
+degree = int(input("Enter an angle in degrees: "))
+radians = math.radians(degree)
+sin_value = math.sin(radians)
+cos_value = math.cos(radians)
+tan_value = math.tan(radians)
+print("sin({degree}°) = {sin_value}")
+print("cos({degree}°) = {cos_value}")
+print("tan({degree}°) = {tan_value}")
+
+#5th question
+import math
+number = int(input("Enter a number: "))
+log_value = math.log(number)
+print(f"The logarithm (base e) of {number} is {log_value}")
+
+
+
+
+
+
+
+
+
