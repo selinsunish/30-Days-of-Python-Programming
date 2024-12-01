99e0a3eb4997c52c25c1d03b07bdfb685b12df1c
DevalakshmiMohandas <devalakshmimohandas@gmail.com>
2024-12-02 00:08:22 +0530

    Add files via upload

---
+++ devalakshmi/sumele.py
@@ -0,0 +1,5 @@
+L = [1, 2, 3, 4, 5]
+
+total_sum = sum(L)
+
+print("The sum of all the elements in the list is:", total_sum)
---
+++ devalakshmi/triangle.py
@@ -0,0 +1,4 @@
+my_list = [1, 2, 3, 4, 5, 6, 7]
+
+for i in range(1, len(my_list) + 1):
+    print(my_list[:i])
