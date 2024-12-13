93e61fe7d98cee286fc6989206e126a8c515e356
Alan-s-benny <alansbenny2028@cs.sjcetpalai.ac.in>
2024-12-13 22:04:08 +0530

    Update Alan s beny

---
+++ Alan s beny/dy2.py
@@ -0,0 +1,37 @@
+n= [1, 2, 3, 4, 5]
+print(3 in n)      
+print(6 not in n)  
+
+x = 25
+y = 25
+z = [1, 2, 3]
+w = [1, 2, 3]
+print(x is y)       
+print(z is w)       
+print(z is not w) 
+
+
+x= input("Enter first string: ")
+y= input("Enter second string: ")
+z = x + " " + y
+print("Resultant string:",z)
+
+
+a = (3 + 6) * 6 - 9 / 3 + 20
+print("output:",a)
+
+
+x = 100
+y = 200
+print("x < y:", x < y)   
+print("x > y:", x > y)   
+print("x <= y:", x <= y) 
+print("x >= y:", x >= y) 
+print("x == y:", x == y) 
+print("x != y:", x != y)
+
+
+
+km = float(input("Enter distance in kilometers: "))
+m = km * 1000
+print((km),"kilometers is equal to",(m),"meters") 
