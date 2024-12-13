5bc49bd3261b3c0417c3559079612625cc9c3604
rishhhane <rishanjmoct15@gmail.com>
2024-12-13 22:31:10 +0530

    Delete rishan joseph/value.py

--- rishan joseph/value.py
+++
@@ -1,9 +0,0 @@
-import math
-deg = float(input("Enter an angle"))
-rad = math.radians(deg)
-sin = math.sin(rad)
-cos = math.cos(rad)
-tan = math.tan(rad)
-print(f"Sin({deg}) = {sin}")
-print(f"Cos({deg}) = {cos}")
-print(f"Tan({deg}) = {tan}")
