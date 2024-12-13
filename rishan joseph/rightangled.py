1cc3009d4afd09fb4a8a46e1fe97ada9cf64aa09
rishhhane <rishanjmoct15@gmail.com>
2024-12-13 22:33:14 +0530

    Add files via upload

---
+++ rishan joseph/rightangled.py
@@ -0,0 +1,5 @@
+import math
+sd1 = int(input("Enter the length of the 1st side"))
+sd2 = int(input("Enter the length of the 2nd side"))
+hypo = math.hypot(sd1, sd2)
+print("The length of the hypotenuse is:", hypo)
