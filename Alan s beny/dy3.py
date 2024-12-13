d64878f67def875d467dd8a28c27f969b3dc050a
Alan-s-benny <alansbenny2028@cs.sjcetpalai.ac.in>
2024-12-13 22:04:48 +0530

    Update Alan s beny

---
+++ Alan s beny/dy3.py
@@ -0,0 +1,39 @@
+import math
+n = float(input("Enter a number: "))
+if n >= 0:
+    s = math.sqrt(n)
+    print("The square root of", (n),"is", (s))
+else:
+    print("invalid value.")
+
+
+n = int(input("Enter a number: "))
+import math
+factorial = math.factorial(n)
+print("The factorial of",(n)," is",
+factorial)
+
+
+a = float(input("Enter the length of one side : "))
+b = float(input("Enter the length of the other side: "))
+import math
+hypotenuse = math.hypot(a, b)
+print("The hypotenuse of the triangle is",(hypotenuse))
+
+
+degree = float(input("Enter an angle:",))
+import math
+radians = math.radians(degree)
+s = math.sin(radians)
+c = math.cos(radians)
+t= math.tan(radians)
+print("Sin",(degree),"° =",(s))
+print("Cos",(degree),"°=",(c))
+print("Tan",(degree),"° =",(t))
+
+
+
+n = float(input("Enter a number: "))
+import math
+l = math.log(n)
+print("The natural logarithm of",(n),"is",(l))
