863c31e556b13f7c5acc123dea27eb941eff333c
Alan-s-benny <alansbenny2028@cs.sjcetpalai.ac.in>
2024-12-13 22:03:03 +0530

    Update Alan s beny

---
+++ Alan s beny/dy1.py
@@ -0,0 +1,38 @@
+n=int(input("enter your age")
+print("Age=",n)
+
+import math
+radius = 10
+area = math.pi * radius ** 2
+print(f"The area of a circle=",area)
+
+n=int(input("Number of years="))
+days=n*365
+weeks=days//7
+months=n*12
+print("Years ",n)
+print("Days ",days)
+print("Weeks",weeks)
+print("Months ",months)
+
+name = input("Enter your name: ")
+department = input("Enter your department: ")
+roll no = input("Enter your roll number: ")
+print("Name:",name)
+print("Department:", department)
+print("Roll Number:",roll no)
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+remainder = num1 % num2
+division = num1 / num2
+floor division = num1 // num2
+print("Sum:",sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Remainder:",remainder)
+print("Division:", division)
+print("Floor Division:"floor division)
