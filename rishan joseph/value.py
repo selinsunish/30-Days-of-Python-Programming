29ae62eee264c38ece4aae42a738993dcaf8b375
rishhhane <rishanjmoct15@gmail.com>
2024-12-13 22:31:24 +0530

    Add files via upload

---
+++ rishan joseph/value.py
@@ -0,0 +1,9 @@
+import math
+deg = float(input("Enter an angle"))
+rad = math.radians(deg)
+sin = math.sin(rad)
+cos = math.cos(rad)
+tan = math.tan(rad)
+print("sin of deg",sin)
+print("cos of deg",cos)
+print("tan of deg",tan)
